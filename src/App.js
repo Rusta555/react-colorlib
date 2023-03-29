651f2c57a0ea8aa02f5cd5186378d16e02376616
Rustam <Rusta555@list.ru>
2023-03-30 00:54:05 +0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,19 +3,25 @@ import Footer from "./components/Footer";
 import Card from "./components/Card";
 import Dynamiccard from "./components/Dynamiccard";
 import {data} from './data/mockdata'
+import { useEffect, useState } from "react";
 
 function App() {
+  const [page, setPage] = useState(1);
+  const [dataState, setDataState] = useState(data.filter((item, i) => i < page * 6).reverse().slice(0, 6))
+
+  useEffect(() => {
+    console.log(page)
+    setDataState(data.filter((item, i) => i < page * 6).reverse().slice(0, 6))
+  }, [page])
+
   return (
   
   <div className="container">
-
-      
-
       <Header />
       
       <Card/>
       <div className="product">{
-        data.map (item=>(<Dynamiccard
+        dataState.map(item=>(<Dynamiccard
           img={item.image}
           title={item.title}
           description={item.description.slice (0, 10)}
@@ -23,15 +29,15 @@ function App() {
           />))
       }
       </div>
-
-      
-      
-
-
+      <div>
+        {Array(Math.ceil(parseFloat(data.length / 6))).fill().map((_, i) => <button
+          onClick={() => {
+            /* console.log('i', i) */
+            setPage(i + 1)
+          }}
+        >{i + 1}</button>)}
+      </div> 
       <Footer />
-
-      
-
     </div>
     );
 }
